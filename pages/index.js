3aa6dc1bc1dec8e744453a98da6b569c1684d299
Gabriel300p <gabriel.andrade200p@gmail.br>
2022-01-06 01:24:38 -0300

    final

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import { getSession } from "next-auth/react";
+import Head from "next/head";
+import Center from "../components/Center";
+import SideBar from "../components/SideBar";
+
+export default function Home() {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <main className="flex">
+        {/* SideBard */}
+        <SideBar />
+
+        {/* Center */}
+        <Center />
+      </main>
+      <div>
+        {/* Player */}
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    }
+  };
+};
